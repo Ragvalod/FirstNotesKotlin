7c33dd5253e2b0d14cbb1e56b2218f480953c21a
Ragvalod <konfucy@gmail.com>
2024-02-27 21:40:05 +0300

    Add TopBar

--- app/src/main/java/com/konfucy/mazuta/firstnoteskotlin/MainActivity.kt
+++ app/src/main/java/com/konfucy/mazuta/firstnoteskotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.konfucy.mazuta.firstnoteskotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,34 +16,65 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.konfucy.mazuta.firstnoteskotlin.ui.theme.FirstNotesKotlinTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+//            val scaffoldState = rememberScaffoldState()
             FirstNotesKotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold {
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstNotesKotlinTheme {
-        Greeting("Android")
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/konfucy/mazuta/firstnoteskotlin/ui_components/MainTopBar.kt
@@ -0,0 +1,8 @@
+package com.konfucy.mazuta.firstnoteskotlin.ui_components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainTopBar(title: String) {
+
+}
